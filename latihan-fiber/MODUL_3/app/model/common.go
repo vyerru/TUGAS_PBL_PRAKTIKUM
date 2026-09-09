baed575219069b55e754ceff97e2092351c4b224
Javier <jrakha55@gmail.com>
2026-09-10 05:57:38 +0700

    pembuatan helper

---
+++ latihan-fiber/MODUL_3/app/model/common.go
@@ -0,0 +1,30 @@
+package model
+
+type ListQuery struct {
+	Page     int
+	Limit    int
+	Sort     string
+	Order    string
+	Search   string
+	IsActive *bool
+}
+
+func (q ListQuery) Offset() int {
+	return (q.Page - 1) * q.Limit
+}
+
+type Meta struct {
+	Page       int `json:"page"`
+	Limit      int `json:"limit"`
+	Total      int `json:"total"`
+	TotalPages int `json:"total_pages"`
+}
+
+// WebResponse adalah bentuk baku seluruh response API.
+type WebResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+	Meta    *Meta  `json:"meta,omitempty"`
+	Errors  any    `json:"errors,omitempty"`
+}
