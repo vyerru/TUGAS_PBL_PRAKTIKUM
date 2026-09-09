35c81135f462b41ef434c0bde19055b202e5debd
Javier <jrakha55@gmail.com>
2026-09-10 05:41:15 +0700

    pembuatan model students

--- latihan-fiber/MODUL_3/app/model/students.go
+++ latihan-fiber/MODUL_3/app/model/students.go
@@ -1,1 +1,30 @@
 package model
+
+import "time"
+
+type Student struct {
+	ID        int       `json:"id"`
+	NIM       string    `json:"nim"`
+	Name      string    `json:"name"`
+	Grade     string    `json:"grade"`
+	IsActive  bool      `json:"is_active"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CreateStudentRequest struct {
+	NIM   string `json:"nim"`
+	Name  string `json:"name"`
+	Grade string `json:"grade"`
+}
+
+type ReplaceStudentRequest struct {
+	Name     string `json:"name"`
+	Grade    string `json:"grade"`
+	IsActive bool   `json:"is_active"`
+}
+
+type PatchStudentRequest struct {
+	Name     *string `json:"name"`
+	Grade    *string `json:"grade"`
+	IsActive *bool   `json:"is_active"`
+}
--- latihan-fiber/MODUL_3/main.go
+++ latihan-fiber/MODUL_3/main.go
@@ -1,1 +1,11 @@
-package modul3
+func main() {
+	config.LoadEnv()
+
+	pool, err := database.NewPool(context.Background())
+	if err != nil {
+		log.Fatalf("database: %v", err)
+	}
+	defer pool.Close()
+
+	// ...bagian Fiber lainnya menyusul di langkah berikutnya
+}
